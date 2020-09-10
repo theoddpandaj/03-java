aae062780fa1a4c4d4a62af5daf9b789e85bfd6b
lazaro guzman <jorrel724@gmai.com>
2020-09-10 18:08:27 -0400

    remade start and made it so it cant proceade without imput

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var all = [];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var spe = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 function generatePassword() {
-  var all = [];
-  var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  var spe = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
-  var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
   var how = prompt("how long should it be?");
-  var uppercase = confirm("include uppercase?");
-  var lowercase = confirm("include lowercase?");
-  var number = confirm("include numbers?");
-  var specail = confirm("include specical casings?");
-  
+  if (!how){
+    alert ("there needs to be a number")
+  }
+  else if (how < 8 || how >128) {
+    alert("needs to be between 8-128");
+     }
+     else{
+      var uppercase = confirm("include uppercase?");
+      var lowercase = confirm("include lowercase?");
+      var number = confirm("include numbers?");
+      var specail = confirm("include specical casings?");
+
+     }
+ 
 
 
-  
+
+
+ 
+
+
+
+
   return "tacotest"
 }
 
