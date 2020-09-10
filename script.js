e014b4657929a35155b8fb3e816108387f4ddbf8
lazaro guzman <jorrel724@gmai.com>
2020-09-09 20:44:17 -0400

    added all var and simple logic on asking qurestions

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var upper =[ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var lower =[ "abcdefghijklmnopqrstuvwxyz"];
+  var numb =[ "0123456789"];
+  var other =["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+  var how = prompt ("how long should it be?");
+  var uppercase = confirm ("include uppercase?");
+  var lowercase = confirm ("include lowercase?");
+  var number = confirm ("include numbers?");
+  var specail = confirm ("include specical casings?");
+
+  return "tacotest"
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
